215d077f6ee8f61091468980894d548bd4769281
kisesy <kisesy@gmail.com>
2017-08-11 23:50:38 +0800

    Update handler_compile.go

--- handler/handler_compile.go
+++ handler/handler_compile.go
@@ -91,6 +91,74 @@ type tagCompare struct {
 			} `json:"parents"`
 		*/
 	} `json:"commits"`
+	MergeBaseCommit struct {
+		Sha    string `json:"sha"`
+		Commit struct {
+			Author struct {
+				Name  string    `json:"name"`
+				Email string    `json:"email"`
+				Date  time.Time `json:"date"`
+			} `json:"author"`
+			Committer struct {
+				Name  string    `json:"name"`
+				Email string    `json:"email"`
+				Date  time.Time `json:"date"`
+			} `json:"committer"`
+			Message string `json:"message"`
+			Tree    struct {
+				Sha string `json:"sha"`
+				URL string `json:"url"`
+			} `json:"tree"`
+			URL          string `json:"url"`
+			CommentCount int    `json:"comment_count"`
+		} `json:"commit"`
+		URL         string `json:"url"`
+		HTMLURL     string `json:"html_url"`
+		CommentsURL string `json:"comments_url"`
+		Author      struct {
+			Login             string `json:"login"`
+			ID                int    `json:"id"`
+			AvatarURL         string `json:"avatar_url"`
+			GravatarID        string `json:"gravatar_id"`
+			URL               string `json:"url"`
+			HTMLURL           string `json:"html_url"`
+			FollowersURL      string `json:"followers_url"`
+			FollowingURL      string `json:"following_url"`
+			GistsURL          string `json:"gists_url"`
+			StarredURL        string `json:"starred_url"`
+			SubscriptionsURL  string `json:"subscriptions_url"`
+			OrganizationsURL  string `json:"organizations_url"`
+			ReposURL          string `json:"repos_url"`
+			EventsURL         string `json:"events_url"`
+			ReceivedEventsURL string `json:"received_events_url"`
+			Type              string `json:"type"`
+			SiteAdmin         bool   `json:"site_admin"`
+		} `json:"author"`
+		Committer struct {
+			Login             string `json:"login"`
+			ID                int    `json:"id"`
+			AvatarURL         string `json:"avatar_url"`
+			GravatarID        string `json:"gravatar_id"`
+			URL               string `json:"url"`
+			HTMLURL           string `json:"html_url"`
+			FollowersURL      string `json:"followers_url"`
+			FollowingURL      string `json:"following_url"`
+			GistsURL          string `json:"gists_url"`
+			StarredURL        string `json:"starred_url"`
+			SubscriptionsURL  string `json:"subscriptions_url"`
+			OrganizationsURL  string `json:"organizations_url"`
+			ReposURL          string `json:"repos_url"`
+			EventsURL         string `json:"events_url"`
+			ReceivedEventsURL string `json:"received_events_url"`
+			Type              string `json:"type"`
+			SiteAdmin         bool   `json:"site_admin"`
+		} `json:"committer"`
+		Parents []struct {
+			Sha     string `json:"sha"`
+			URL     string `json:"url"`
+			HTMLURL string `json:"html_url"`
+		} `json:"parents"`
+	} `json:"merge_base_commit"`
 }
 type latestRelease struct {
 	TagName         string `json:"tag_name"`
@@ -128,24 +196,30 @@ func gen_desc(Package string) (desc string, err error) {
 	user, repo := ps[1], ps[2]
 
 	// "https://api.github.com/repos/:owner/:repo/releases/latest"
-	apiurl := "https://api.github.com/repos/%s/%s/releases/latest"
+	// apiurl := "https://api.github.com/repos/%s/%s/releases/latest"
+	// GET /repos/:owner/:repo/releases
+	apiurl := "https://api.github.com/repos/%s/%s/releases"
 	apiurl = fmt.Sprintf(apiurl, user, repo)
 
-	var lr latestRelease
 	data, err := httpGet(apiurl)
 	if err != nil {
 		panic(err)
 	}
+	var lr []latestRelease
 	err = json.Unmarshal(data, &lr)
 	if err != nil {
 		return
+	}
+	pre := 0
+	if len(lr) > 1 {
+		pre = 1
 	}
 
 	// lr.TagName = "1.2"
 
 	// "GET /repos/:owner/:repo/compare/:base...:head"
 	apiurl = "https://api.github.com/repos/%s/%s/compare/%s...%s"
-	apiurl = fmt.Sprintf(apiurl, user, repo, lr.TagName, lr.TargetCommitish)
+	apiurl = fmt.Sprintf(apiurl, user, repo, lr[pre].TagName, lr[pre].TargetCommitish)
 	data, err = httpGet(apiurl)
 	if err != nil {
 		return
@@ -174,7 +248,12 @@ func gen_desc(Package string) (desc string, err error) {
 			b.WriteString(fmt.Sprintf("[`%s`](%s) %s\n", commit_hash, commit_url, message))
 		}
 	} else {
-		return
+		mc := tc.MergeBaseCommit
+		commit_url := mc.Parents[0].HTMLURL
+		commit_hash := mc.Sha[:7]
+		message := mc.Commit.Message
+		// msg = '[{user_name}]({user_link}): [`{commit_sha1}`]({commit_url}) {message}'.format(**locals())
+		b.WriteString(fmt.Sprintf("[`%s`](%s) %s\n", commit_hash, commit_url, message))
 	}
 	return b.String(), nil
 }
@@ -185,6 +264,7 @@ var tempBuild = path.Join(os.TempDir(), "cloudgox")
 //server's compile method
 func (s *goxHandler) compile(c *Compilation) error {
 	s.Printf("compiling %s...\n", c.Package)
+	s.Printf("version %s\n", c.Version)
 	c.StartedAt = time.Now()
 	//optional releaser
 	releaser := s.releasers[c.Releaser]
